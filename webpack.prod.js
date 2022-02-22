537fc9e0647cb6121c370e08d18a197bbe155466
onnanoco <moe@onnano.co>
2022-02-22 20:10:09 +0900

    migrate

--- webpack.prod.js
+++
@@ -1,9 +0,0 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common');
-// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-
-module.exports = merge(common, {
-  mode: 'production',
-  devtool: false,
-  // plugins: [new BundleAnalyzerPlugin()],
-});
